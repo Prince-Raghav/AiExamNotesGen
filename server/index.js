de78561c4d2b50b79394363a21b9fb0f153ddf51
Prince <princeraghav150@gmail.com>
2026-06-27 21:34:15 +0530

    Fix CORS for production

--- server/index.js
+++ server/index.js
@@ -22,13 +22,11 @@ app.post(
   stripeWebhook
 );
 
-app.use(cors(
-    {origin:["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
-        credentials:true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    }
-))
-
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}))
 
 
 app.use(express.json())
